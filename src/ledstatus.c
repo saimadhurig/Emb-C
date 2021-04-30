18eea547730436ba195e71553a8d71432a048881
saimadhurig <81443037+saimadhurig@users.noreply.github.com>
2021-05-01 01:59:33 +0530

    Update ledstatus.c

--- src/ledstatus.c
+++ src/ledstatus.c
@@ -1,1 +1,30 @@
 
+/**
+ * @file ledstatus.c
+ * @author sai madhuri g
+ * @brief Chanfes the state of LED
+ * @version 0.1
+ * @date 2021-04-27
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
+// include files
+#include<avr/io.h>
+#include "ledstatus.h"
+/**
+ * @brief Change the state of the LED Pin according to the value of state
+ * 
+ * @param state Pin level to which the LED Pin should be set
+ */
+void ledstatus(uint8_t state)
+{
+	LED_PORT = (state << LED_PIN);
+}
+void InitLED(void)
+{
+	DDRB|=(1<<PB1);
+    DDRD&=~(1<<PD1);
+    PORTD|=(1<<PD1);
+    PORTD|=(1<<PD2);
+}
