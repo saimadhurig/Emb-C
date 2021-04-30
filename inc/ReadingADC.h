c56b49105deba95a486f43a868af4a3f8cde5243
saimadhurig <81443037+saimadhurig@users.noreply.github.com>
2021-05-01 02:23:29 +0530

    Create ReadingADC.h

---
+++ inc/ReadingADC.h
@@ -0,0 +1,22 @@
+/**
+ * @file ReadingADC.h
+ * @author sai madhuri g
+ * @brief ADC function definition
+ * @version 0.1
+ * @date 2021-04-29
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
+
+#ifndef __READINGADC_H__
+#define __READINGADC_H__
+/**
+ * @brief Reading the analog input and returning a digital 8bit value
+ * 
+ * @param ch 
+ * @return uint16_t 
+ */
+uint16_t ReadingADC(uint8_t ch);
+void InitialiseADC();
+#endif
