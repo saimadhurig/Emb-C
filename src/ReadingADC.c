388aac8570a96ac6707489fe0a5beafedbb1cf57
saimadhurig <81443037+saimadhurig@users.noreply.github.com>
2021-05-01 02:33:12 +0530

    Create ReadingADC.c

---
+++ src/ReadingADC.c
@@ -0,0 +1,33 @@
+/**
+ * @file ReadingADC.c
+ * @author sai madhuri g
+ * @brief 
+ * @version 0.1
+ * @date 2021-04-29
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
+#include<avr/io.h>
+#include "ReadingADC.h"
+
+void InitialiseADC()
+{
+    ADMUX=(1<<REFS0);
+    ADCSRA=(1<<ADEN)|(7<<ADPS0);
+    
+}
+uint16_t ReadingADC(uint8_t cha)
+{
+    
+    ADMUX&=0xf8;
+    cha=cha&0b00000111;
+    ADMUX|=cha;
+    
+    ADCSRA|=(1<<ADSC);
+    
+    while(!(ADCSRA & (1<<ADIF)));
+   
+    ADCSRA|=(1<<ADIF);
+    return(ADC);
+}
